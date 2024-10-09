b8d9c9a36dc6a64d4f1cb715cd65908e77656259
Ali Niaz <97086841+AliNiaz990487@users.noreply.github.com>
2024-10-10 00:12:53 +0500

    fixed scrolling issue

--- lib/productCards/meats.dart
+++ lib/productCards/meats.dart
@@ -4,51 +4,60 @@ import '../variables.dart';
 import 'product_card.dart';
 import 'product_page.dart';
 
-class MeatCards extends StatelessWidget{
+class MeatCards extends StatelessWidget {
   const MeatCards({super.key, required this.homePage});
 
   final HomePage homePage;
-    productPage(BuildContext context, int index){
+
+  void productPage(BuildContext context, int index) {
     Navigator.push(
-      context, 
-      MaterialPageRoute(builder: (context)=>ProductPage(
-        homePage: homePage,
-        description: meatDescriptions[index], sku: 'me-$index', index: index, products: meats,))
-    );
+        context,
+        MaterialPageRoute(
+            builder: (context) => ProductPage(
+                  homePage: homePage,
+                  description: meatDescriptions[index],
+                  sku: 'me-$index',
+                  index: index,
+                  products: meats,
+                )));
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ListView.builder(
+      physics: NeverScrollableScrollPhysics(), // Disable scrolling inside ListView
       shrinkWrap: true,
-      itemCount: meats.length~/2,
+      itemCount: meats.length ~/ 2,
       itemBuilder: (context, index) {
         int column1 = (index * 2);
-        int column2 = (index * 2)+1;
-        return Row(       //   1 Row
+        int column2 = (index * 2) + 1;
+        return Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Expanded(
               child: InkWell(
-                onTap: (){productPage(context, column1);},
+                onTap: () {
+                  productPage(context, column1);
+                },
                 child: ProductCard(
-                    product: meats[column1],
-                    sku: 'me-$column1',
-                    homePage: homePage,
-                    ),
+                  product: meats[column1],
+                  sku: 'me-$column1',
+                  homePage: homePage,
+                ),
               ),
             ),
-                
             Expanded(
               child: InkWell(
-                onTap: (){productPage(context, column2);},
+                onTap: () {
+                  productPage(context, column2);
+                },
                 child: ProductCard(
                   product: meats[column2],
                   sku: 'me-$column2',
-                  homePage: homePage
+                  homePage: homePage,
                 ),
               ),
-            )
+            ),
           ],
         );
       },
