894731c3e9bbef38aa0e82b9d98d26727f645127
Ali Niaz <97086841+AliNiaz990487@users.noreply.github.com>
2024-10-10 00:11:48 +0500

    fixed scrolling issue

--- lib/productCards/vegetables.dart
+++ lib/productCards/vegetables.dart
@@ -4,56 +4,63 @@ import '../variables.dart';
 import 'product_card.dart';
 import 'product_page.dart';
 
-class VegetableCards extends StatelessWidget{
+class VegetableCards extends StatelessWidget {
   const VegetableCards({super.key, required this.homePage});
 
   final HomePage homePage;
-  productPage(BuildContext context, int index){
+
+  void productPage(BuildContext context, int index) {
     Navigator.push(
-      context, 
-      MaterialPageRoute(builder: (context)=>ProductPage(
-        homePage: homePage,
-        description: vegetableDescriptions[index], sku: 've-$index', index: index, products: vegetables,))
-    );
+        context,
+        MaterialPageRoute(
+            builder: (context) => ProductPage(
+                  homePage: homePage,
+                  description: vegetableDescriptions[index],
+                  sku: 've-$index',
+                  index: index,
+                  products: vegetables,
+                )));
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ListView.builder(
+      physics: NeverScrollableScrollPhysics(), // Disable scrolling inside ListView
       shrinkWrap: true,
-      itemCount: vegetables.length~/2,
+      itemCount: vegetables.length ~/ 2,
       itemBuilder: (context, index) {
         int column1 = (index * 2);
-        int column2 = (index * 2)+1;
-        return Row(       //   1 Row
+        int column2 = (index * 2) + 1;
+        return Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Expanded(
               child: InkWell(
-                onTap: (){productPage(context, column1);},
+                onTap: () {
+                  productPage(context, column1);
+                },
                 child: ProductCard(
-                    product: vegetables[column1],
-                    sku: 've-$column1', 
-                    homePage: homePage,
-                    ),
+                  product: vegetables[column1],
+                  sku: 've-$column1',
+                  homePage: homePage,
+                ),
               ),
             ),
-                
             Expanded(
               child: InkWell(
-                onTap: (){productPage(context, column2);},
+                onTap: () {
+                  productPage(context, column2);
+                },
                 child: ProductCard(
                   product: vegetables[column2],
                   sku: 've-$column2',
                   homePage: homePage,
                 ),
               ),
-            )
+            ),
           ],
         );
       },
     );
   }
 }
-
-
