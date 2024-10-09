a0846137698c45d2108983ab99779e19888d971f
Ali Niaz <97086841+AliNiaz990487@users.noreply.github.com>
2024-10-10 00:07:47 +0500

    fixed scrolling issue

--- lib/productCards/fruits.dart
+++ lib/productCards/fruits.dart
@@ -4,44 +4,53 @@ import 'package:grocery_app/productCards/product_page.dart';
 import '../variables.dart';
 import 'product_card.dart';
 
-class FruitCards extends StatelessWidget{
+class FruitCards extends StatelessWidget {
   const FruitCards({super.key, required this.homePage});
 
   final HomePage homePage;
-  productPage(BuildContext context, int index){
+  productPage(BuildContext context, int index) {
     Navigator.push(
-      context, 
-      MaterialPageRoute(builder: (context)=>ProductPage(
-        homePage: homePage,
-        description: fruitDescriptions[index], sku: 'fu-$index', index: index, products: fruits,))
-    );
+        context,
+        MaterialPageRoute(
+            builder: (context) => ProductPage(
+                  homePage: homePage,
+                  description: fruitDescriptions[index],
+                  sku: 'fu-$index',
+                  index: index,
+                  products: fruits,
+                )));
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ListView.builder(
+      physics:
+          NeverScrollableScrollPhysics(), // Disable scrolling inside ListView
       shrinkWrap: true,
-      itemCount: fruits.length~/2,
+      itemCount: fruits.length ~/ 2,
       itemBuilder: (context, index) {
         int column1 = (index * 2);
-        int column2 = (index * 2)+1;
-        return Row(       //   1 Row
+        int column2 = (index * 2) + 1;
+        return Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Expanded(
               child: InkWell(
-                onTap: (){productPage(context, column1);},
+                onTap: () {
+                  productPage(context, column1);
+                },
                 child: ProductCard(
-                    product: fruits[column1],
-                    sku: 'fu-$column1',
-                    homePage: homePage,
-                    ),
+                  product: fruits[column1],
+                  sku: 'fu-$column1',
+                  homePage: homePage,
+                ),
               ),
             ),
-                
             Expanded(
               child: InkWell(
-                onTap: (){productPage(context, column2);},
+                onTap: () {
+                  productPage(context, column2);
+                },
                 child: ProductCard(
                   product: fruits[column2],
                   sku: 'fu-$column2',
